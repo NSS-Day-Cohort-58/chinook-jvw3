bbf3a55b41f21e9f0099cbef7250734000145c21
Jonathan Woodard <jvw.226@gmail.com>
2022-11-28 11:33:42 -0600

    first exercise complete

---
+++ non_usa_customers.sql
@@ -0,0 +1,11 @@
+/*
+Provide a query showing Customers who are not in the US. The resultant table should include:
+Customer's full name
+Customer Id,
+Customer's country
+*/
+
+SELECT CustomerId, FirstName, LastName, Country FROM CUSTOMER
+WHERE Country<>"USA"
+
+
